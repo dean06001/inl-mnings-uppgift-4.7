19195daa87d269d08b59acd1d3dfa63b17d62eff
dean06001 <dean06001@E-PW0234KC>
2023-10-19 15:15:21 +0200

    Add project files.

---
+++ inlämnings uppgift 4.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("Hur mång steg till höger ska meddelandet flyttas?");
+            int steg = int.Parse(Console.ReadLine());
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
